f44d8f68b21aa85afdbea1c0a283ea7433e362d6
Mariana Santilli <santillim3@gmail.com>
2023-04-16 18:50:07 -0300

    BaSP-W05-2023:Excercises variables and operators

---
+++ WEEK-05/Excercises/01-variables-and-operators.js
@@ -0,0 +1,19 @@
+// excersice a
+
+var age= 28;
+var YearOfBirth= 1995;
+var addition= age + YearOfBirth;
+console.log('the total is:' + addition);
+
+//excersice b
+
+var firstName= 'Mariana';
+var gender= 'F';
+console.log('My name is:'+ firstName + 'and my gender is:' + gender);
+
+//excercise c
+
+var userNamme= 'Mari';
+var password= 'santillimari';
+var totalLenght= userNamme.length + password.length;
+console.log ('the total lenght is:' + totalLenght);
