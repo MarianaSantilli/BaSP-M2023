14c9b42a1336bf82996bfadd6755f42f7d104dad
Mariana Santilli <santillim3@gmail.com>
2023-04-17 23:17:22 -0300

    BaSP-W05-2023: Excercises functions

---
+++ WEEK-05/Excercises/06-functions.js
@@ -0,0 +1,52 @@
+//Excercise a
+
+function addition (a, b){
+
+    return a + b;
+}
+
+var totalAddition= addition(90, 65)
+console.log(totalAddition);
+
+//Excercise b
+
+function addition (a, b){
+    if(typeof a !== 'number' || typeof b !== 'number' ){
+        alert('One of the parameters is not a number');
+        return NaN
+    }else{
+        return a + b;
+    }
+}
+console.log(addition (90, 'number'))
+
+//Excercise c
+
+function validateInteger (a){
+    var validation= a % 1;
+    if(validation === 0 ){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(validateInteger (12));
+
+//Excercise d
+
+function secondValidation (a, b){
+    if(typeof a !== 'number' || typeof b !== 'number' ){
+        alert('One of the parameters is not a number');
+        return NaN
+    }else if(!validateInteger (a) || !validateInteger (b)){
+        alert ('Error');
+        a = Math.round (a);
+        b = Math.round (b);
+    }
+    return a + b;
+}
+
+console.log(secondValidation (42, 22.3))
+
+//Excercise e
